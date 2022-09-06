163e91676f021f5282dbd9ecb081e96fbb149a3e
MinPro <1MinPro1@gmail.com>
2022-09-06 11:29:18 +0100

    add Print

---
+++ test.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://google.com")
+
+time.sleep(5)
+
+print(def test_description(self):
+    # TODO: write code...)
